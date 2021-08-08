1b033cfc2405dbb8f1be5e84229a3144aa30e0fc
JoseRodriguezF <ejrodriguezf@correo.udistrital.edu.co>
2021-08-08 15:57:49 -0500

    new db keys

--- src/keys.ts
+++ src/keys.ts
@@ -7,11 +7,20 @@
     }
 }*/
 
-export default{
+/*export default{
     database: {
         host: 'localhost',
         user: 'ieieuser',
         password: 'IeIe/2021#',
         database: 'ieiedb',
     }
+}*/
+export default{
+    database: {
+        host: 'ckshdphy86qnz0bj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'rbj4bt0qwz1vsl8v',
+        password: 'oxpwi9czlkana9ip',
+        database: 'tkn1ritt5z5z8lpc',
+    }
 }
+
